081aa8ed6c0a6fce7c960dae2a291379e2953815
aailyin <antonaleksei.ilyin@gmail.com>
2016-08-21 19:59:42 +0300

    Added background and content js. Need to finish

---
+++ js/content.js
@@ -0,0 +1,11 @@
+const currentTab = location.origin;
+let timeoutId = null;
+
+// The current tab is inactive so we need to remove timeout
+window.onblur = function () {
+
+};
+
+window.onfocus = function () {
+
+};

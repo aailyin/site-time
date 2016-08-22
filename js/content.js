c0c215e6909df419107f461f5fcecaa4c82f7312
aailyin <antonaleksei.ilyin@gmail.com>
2016-08-22 21:40:30 +0300

    Added content js file

--- js/content.js
+++ js/content.js
@@ -1,11 +1,29 @@
 const currentTab = location.origin;
+const ADD_TIME = 'ADD_TIME';
+const GET_TIME = 'GET_TIME';
+
 let timeoutId = null;
 
 // The current tab is inactive so we need to remove timeout
 window.onblur = function () {
-
+  clearTimeout(timeoutId);
+  timeoutId = null;
 };
 
 window.onfocus = function () {
-
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(function () {
+    chrome.runtime.sendMessage({
+      action: ADD_TIME, 
+      data: {
+        tabId: currentTab,
+        time: 60000
+      }
+    }, function (data) {
+      // data was sent
+      console.log(data);
+    });
+  }, 60000);
 };
